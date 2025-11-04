5db7771b7ed1681686f5e4fb7d566e82e356b1ad
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-11-04 20:55:11 +0100

    Add files via upload

---
+++ hpc/AF3complex/genome_kmer_melting.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# genome_melting.sh
+# This script runs PAQman/samtools/bedtools on FASTA files transferred via HTCondor
+
+# Exit on error
+set -euo pipefail
+
+# Parameters (could also pass via HTCondor submit if you want)
+kmersize=8 
+
+#loop over the genome
+
+ls | grep "\.fa$\|.fasta$" | while read genome
+do
+    name=$(basename "$genome" | sed 's/\.[^.]*$//')
+
+    meryl k=${kmersize} count ${genome} output ${name}_k8.meryl
+    meryl print ${name}_k8.meryl | grep -vE "(AAAA|CCCC|GGGG|TTTT|ATATAT|TATATA|GCGCGC|CGCGCG)" > ${name}_k8.and_count.txt
+    cat ${name}_k8.and_count.txt | shuf | cut -f1 | awk 'BEGIN{srand()} !/^$/ {if(rand() <= 0.5) print $0}' > ${name}_k8.random_half.txt 
+    cat ${name}_k8.random_half.txt | awk '{print ">"$0"\n"$0}' > ${name}_k8.random_half.fa
+
+done
